17f5e5a30c5cc56a2437f3ae063bb19c86126147
Данил Ченцов <danil090598@gmail.com>
2019-12-01 18:25:44 +0300

    Skeleton

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
